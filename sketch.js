8194103e6dd2aacd5f45b82ed0612226eac9436c
andybartelsman <aabartel@uc.cl>
2016-12-12 20:30:42 -0300

    Imagen de fondo

--- sketch.js
+++ sketch.js
@@ -1,18 +1,43 @@
-//2016_08_16
+//2016_12_12
+
+var bg;
+var y = 0;
 
 function setup() {
-
+ bg= loadImage("images/ladertiag5.jpg");
+ createCanvas(720,400);
+ 
 //crear un lienzo
 //CreateCanvas(hor,ver)
 createCanvas(windowWidth, windowHeight);
 
 //pintar el fondo naranjo
 //background(color);
-background(255,145,28);
+//background(255,145,28);
 
 
 }
 
 function draw() {
+  background(bg);
+  stroke(226,204,0);
+  line(0,y,width,y)
+  
+  y++;
+  if (y > height) {
+    y = 0;
+  }
+
+//escribir texto
+textFont ("Helvetica");
+textSize(14);
+text("Andres Bartelsman Vega is a chilean Architect who is currently learning how to develop this web page coding by himself.",50,50);
+strokeWeight (0)
+fill(0);
+
+//s="Andres Bartelsman Vega is a chilean Architect who is learning how to develop a web page coding by himself.";
+//fill (50);
+//text (s,10,10,70,80); 
+
   
 }
