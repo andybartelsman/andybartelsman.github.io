9b7187cabcbb333cfdd6b9796eb43a19192d9106
andybartelsman <aabartel@uc.cl>
2018-03-07 10:33:35 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,12 +33,16 @@ textFont ("Helvetica");
 textSize(12);
 text("Andrés Bartelsman Vega",50,50);
 text("Born on July 15th, 1989 in Santiago de Chile",50,65);
-text("Residence: Ricardo Lyon 2212, Santiago de Chile",50,80);
+text("Santiago de Chile",50,80);
 text("Nationality: Chilean / Dutch",50,95);
 text("Contact: aabartel@uc.cl",50,110);
 text("Studies",50,140);
 text("2008-2016 Architecture Studies (professional degree) at Universidad Católica de Chile / Santiago, Chile",50,155);
 text("1994-2007 Primary & Secondary Education at Deutsche Schule Santiago / Santiago, Chile",50,170);
+text("Languages",50,200);
+text("Spanish Native",50,215);
+text("English Intermediate",50,230);
+text("German Intermediate",50,245); 
 strokeWeight (0)
 fill(250,250,250);
 
